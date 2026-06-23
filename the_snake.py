d77e64b18633320666aa7bfdb92093eb3bb38793
MerciliantChemera <thecitrus228@yandex.ru>
2026-06-23 18:24:50 +0300

    fix more errors

--- the_snake.py
+++ the_snake.py
@@ -7,7 +7,7 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
-GRID_CENTER = (GRID_WIDTH // 2, GRID_HEIGHT // 2)
+GRID_CENTER: tuple[int, int] = (GRID_WIDTH // 2, GRID_HEIGHT // 2)
 
 # Сложность игры (скорость змейки)
 SPEED = 6
@@ -77,8 +77,9 @@ class GameObject:
     ) -> None:
         """Функция для отрисовки на экран одной ячейки заданного цвета."""
         color = color or self.body_color
+        # Переводим из координат поля в координаты окна
         rect = pg.Rect(
-            tuple(i * GRID_SIZE for i in position),
+            (position[0] * GRID_SIZE, position[1] * GRID_SIZE),
             (GRID_SIZE, GRID_SIZE)
         )
         pg.draw.rect(screen, color, rect)
@@ -91,24 +92,22 @@ class Apple(GameObject):
 
     def __init__(
         self,
-        color: tuple[int, int, int] = APPLE_COLOR,
-        exclude_positions: list[tuple[int, int]] | None = None
+        exclude_positions: list[tuple[int, int]],
+        color: tuple[int, int, int] = APPLE_COLOR
     ) -> None:
         super().__init__(color=color)
-        if exclude_positions is None:
-            exclude_positions = [GRID_CENTER]
         self.randomize_position(exclude_positions)
 
     def randomize_position(
         self,
-        excluded_positions: list[tuple[int, int]] | None
+        excluded_positions: list[tuple[int, int]]
     ) -> None:
         """Задаёт новую случайную позицию для яблока."""
-        if excluded_positions is not None:
-            while self.position in excluded_positions:
-                random_x = randint(0, GRID_WIDTH - 1)
-                random_y = randint(0, GRID_HEIGHT - 1)
-                self.position = (random_x, random_y)
+        self.position = excluded_positions[0]
+        while self.position in excluded_positions:
+            random_x = randint(0, GRID_WIDTH - 1)
+            random_y = randint(0, GRID_HEIGHT - 1)
+            self.position = (random_x, random_y)
 
     def draw(self) -> None:
         """Отрисовывает яблоко в его текущей позиции."""
@@ -123,17 +122,7 @@ class Snake(GameObject):
         color: tuple[int, int, int] = SNAKE_COLOR
     ) -> None:
         super().__init__(color=color)
-        self.position = GRID_CENTER
         self.reset()
-
-    @staticmethod
-    def check_pause(func):
-        """Декоратор, обеспечивающий паузу."""
-        def wrapper(self, *args, **kwargs):
-            if not self._pause:
-                func(self, *args, **kwargs)
-            return None
-        return wrapper
 
     def reset(self) -> None:
         """Сброс змейки к начальным параметрам."""
@@ -142,17 +131,14 @@ class Snake(GameObject):
         self.direction = RIGHT
         self.next_direction = None
         self._last = None
-        self._pause: bool = False
 
     def draw(self) -> None:
         """Отрисовка змейки."""
         self._draw_cell(self.get_head_position, need_border=True)
 
-        if self._last:
+        if self._last and self._last not in self.positions:
             self._draw_cell(self._last, color=BOARD_BACKGROUND_COLOR)
-            self._last = None
 
-    @check_pause
     def move(self) -> None:
         """Перемещает змейку в текущем направлении движения."""
         new_position = (
@@ -162,13 +148,14 @@ class Snake(GameObject):
         self.positions.insert(0, new_position)
         if self.length < len(self.positions):
             self._last = self.positions.pop()
+        else:
+            self._last = None
 
     @property
-    def get_head_position(self) -> None:
+    def get_head_position(self) -> tuple[int, int]:
         """Возвращает текущее положение головы змейки."""
         return self.positions[0]
 
-    @check_pause
     def update_direction(self) -> None:
         """Проверяет, была ли попытка изменения направления движения.
 
@@ -191,12 +178,6 @@ class Snake(GameObject):
         """
         self._pause = not self._pause
 
-    def update_direction(self) -> None:
-        """Проверяет, была ли попытка изменения направления движения."""
-        if self.next_direction:
-            self.direction = self.next_direction
-            self.next_direction = None
-
 
 def main():
     """Основное тело программы."""
@@ -204,34 +185,38 @@ def main():
     screen.fill(BOARD_BACKGROUND_COLOR)
 
     snake = Snake()
-    apple = Apple()
+    apple = Apple(snake.positions)
 
     ticks_per_second = SPEED
+    pause = False
 
     while True:
         clock.tick(ticks_per_second)
 
-        handle_keys(snake)
-        snake.update_direction()
-        snake.move()
+        handle_keys(snake, pause)
+        if not pause:
+            snake.update_direction()
+            snake.move()
 
-        if snake.get_head_position == apple.position:
-            snake.length += 1
-            apple.randomize_position(excluded_positions=snake.positions)
-            if INCREASE_SPEED_DURING_GAME:
-                ticks_per_second += SPEED_INCREMENT
-        elif snake.get_head_position in snake.positions[1:]:
-            # Поражение: змейка врезалась в себя.
-            screen.fill(BOARD_BACKGROUND_COLOR)
-            snake.reset()
-            apple.randomize_position(excluded_positions=snake.positions)
+            if snake.get_head_position == apple.position:
+                snake.length += 1
+                apple.randomize_position(excluded_positions=snake.positions)
+                if INCREASE_SPEED_DURING_GAME:
+                    ticks_per_second += SPEED_INCREMENT
+            elif snake.get_head_position in snake.positions[1:]:
+                # Поражение: змейка врезалась в себя.
+                if INCREASE_SPEED_DURING_GAME:
+                    ticks_per_second = SPEED
+                screen.fill(BOARD_BACKGROUND_COLOR)
+                snake.reset()
+                apple.randomize_position(excluded_positions=snake.positions)
 
-        snake.draw()
-        apple.draw()
-        pg.display.update()
+            snake.draw()
+            apple.draw()
+            pg.display.update()
 
 
-def handle_keys(game_object: GameObject) -> None:
+def handle_keys(snake: Snake, pause: bool) -> None:
     """Функция обработки действий пользователя.
 
     Управление змейкой реализовано при помощи 'клавиш-стрелок'.
@@ -245,13 +230,13 @@ def handle_keys(game_object: GameObject) -> None:
         if event.type == pg.KEYDOWN:
             # Поворот змейки
             if event.key in (pg.K_UP, pg.K_DOWN, pg.K_LEFT, pg.K_RIGHT):
-                direction_info = UPDATE_ROTATION.get(event.key)
-                if game_object.direction != direction_info['conflict']:
-                    game_object.next_direction = direction_info['direction']
+                direction_info = UPDATE_ROTATION[event.key]
+                if snake.direction != direction_info['conflict']:
+                    snake.next_direction = direction_info['direction']
             # Пауза
             if event.key == pg.K_SPACE:
-                game_object.next_direction = None
-                game_object.switch_pause()
+                snake.next_direction = None
+                pause = not pause
 
 
 if __name__ == '__main__':
